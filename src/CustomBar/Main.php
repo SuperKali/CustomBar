74c2e3cbbafd84cb9b4f2c6e896f1c12650c4de5
SuperKali <danychannel18@gmail.com>
2017-08-16 23:48:37 +0200

    fix and solved the config problem

--- src/CustomBar/Main.php
+++ src/CustomBar/Main.php
@@ -3,57 +3,43 @@
 namespace CustomBar;
 
 use pocketmine\event\Listener;
-use pocketmine\Server;
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\Plugin;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\scheduler\Task;
-use pocketmine\utils\Config;
+
 
 use CustomBar\Task\UpdateHud as UH;
 
 class Main extends PluginBase implements Listener
 {
-    public $config;
 
     public function onEnable()
     {
+        $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("CustomBar Enable");
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new UH($this), 20);
-        /*foreach ($this->getServer()->getPluginManager()->getPlugins() as $p) {
-            if (strpos($p->getName(), strcasecmp("text", "TEXT"))) {
-                $this->getLogger()->notice("Conflict plugin detected!");
-                $this->getLogger()->notice("Please remove plugin '$p' to make CustomBar work!");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            }
-        }*/
+        $this->getLogger()->info("CustomBar by SuperKali Enable");
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new UH($this), $this->getConfig()->get("time"));
     }
 
     public function onDisable()
     {
-        $this->getLogger()->info("CustomBar Disable");
+        $this->getLogger()->info("CustomBar by SuperKali Disable");
     }
 
-    public function formatHUD(): string{
+    public function formatHUD(): string
+    {
         return str_replace(array(
-            "&", // 1
-            "{tps}", // 2
-            "{motd}", // 3
-            "{players}", // 4
-            "{max_players}", // 5
-            "{server_name}", // 6
-            "{line}" // 7
+            "&",
+            "{tps}",
+            "{load}",
+            "{players}",
+            "{max_players}",
+            "{line}",
         ), array(
-            "§", // 1
-            $this->getServer()->getTicksPerSecond(), // 2
-            $this->getServer()->getMotd(), // 3
-            count($this->getServer()->getOnlinePlayers()), // 4
-            $this->getServer()->getMaxPlayers(), // 5
-            $this->getServer()->getName(), // 6
-            "\n" // 7
-        ));
+            "§",
+            $this->getServer()->getTicksPerSecond(),
+            $this->getServer()->getTickUsage(),
+            count($this->getServer()->getOnlinePlayers()),
+            $this->getServer()->getMaxPlayers(),
+            "\n"
+        ), $this->getConfig()->getNested("text"));
     }
 }
--- src/CustomBar/Task/UpdateHud.php
+++ src/CustomBar/Task/UpdateHud.php
@@ -3,9 +3,7 @@
 namespace CustomBar\Task;
 
 use pocketmine\scheduler\PluginTask;
-use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\plugin\Plugin;
 
 
 class UpdateHud extends PluginTask
@@ -18,11 +16,11 @@ class UpdateHud extends PluginTask
 
     public function onRun($tick)
     {
-        int $hud = $this->plugin->formatHUD();
+        $text = $this->plugin->formatHUD();
         //$cfg = $this->plugin->config["text"]; //Thanks Fycarman For Fix
         $pl = $this->plugin->getServer()->getOnlinePlayers();
         foreach ($pl as $p) {
-            $p->sendPopup($hud);
+            $p->sendPopup($text);
         }
     }
 }
